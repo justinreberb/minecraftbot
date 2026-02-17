82d2a85b0206d89a8c7062741d992c66dfa30123
reverbexe <anmolrajbhandari35@gmail.com>
2026-02-17 18:48:34 +0545

    first program

--- main.cpp
+++ main.cpp
@@ -8,25 +8,61 @@
 // }//this is my first program
 // //test
 
+// #include <iostream>
+
+// using namespace std;
+// int a;
+// int b;
+// int sum;
+// void s();
+
+// int main(){
+
+//     cout<<"Enter your first number"<<endl;
+//     cin>>a;
+//     cout<<"Enter your second number"<<endl;
+//     cin>>b;
+
+//     s();
+// }
+
+// void s(){
+//  sum = a + b;
+//     cout<<sum<<endl;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int arr[5] = {1,2,3,4,5};
+//     int sum = 0;  // move outside loop
+
+//     for(int i = 0 ; i < 5 ; i++){
+//         sum += arr[i];
+//     }
+
+//     cout << "Sum = " << sum << endl;
+
+//     return 0;
+// }
 #include <iostream>
-
 using namespace std;
-int a;
-int b;
-int sum;
-void s();
 
-int main(){
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    
+    int sum = 0;
+    for(int i = 0 ;i < 5 ;i++ ){
 
-    cout<<"Enter your first number"<<endl;
-    cin>>a;
-    cout<<"Enter your second number"<<endl;
-    cin>>b;
+        sum += arr[i];
 
-    s();
+
+
+    } 
+    double avg = sum /2 ;
+    cout<<avg;
 }
 
-void s(){
- sum = a + b;
-    cout<<sum<<endl;
-}
---
+++ my first project/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+double marks[5];
+int sum = 0;
+void marksLoop();
+void inputTaker();
+
+int main()
+{
+
+    inputTaker();
+    marksLoop();
+    double total = sum;
+    double avg = sum / 5;
+    std::cout << total<<std::endl;
+
+    std::cout << avg<<std::endl;
+}
+
+void marksLoop()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        sum += marks[i];
+    }
+}
+
+void inputTaker()
+{
+
+    std::cout << "Enter the marks: " << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cin >> marks[i];
+    }
+}
